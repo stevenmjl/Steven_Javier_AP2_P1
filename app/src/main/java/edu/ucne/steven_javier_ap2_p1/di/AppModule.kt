c82fd5d770292ed4afe9fd108f31dfcb448e9227
stevenmjl <stevenmjl.24@gmail.com>
2026-02-04 18:57:17 -0400

    Agregando AppModule.kt

---
+++ app/src/main/java/edu/ucne/steven_javier_ap2_p1/di/AppModule.kt
@@ -0,0 +1,11 @@
+package edu.ucne.steven_javier_ap2_p1.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+}
