2d2ffad0f1f0b048390aa68cd390de2a0e2d99b6
stevenmjl <stevenmjl.24@gmail.com>
2026-02-04 20:08:00 -0400

    Agregando ListScreen con su preview

---
+++ app/src/main/java/edu/ucne/steven_javier_ap2_p1/presentation/entidad/list/BorrameListScreen.kt
@@ -0,0 +1,20 @@
+package edu.ucne.steven_javier_ap2_p1.presentation.entidad.list
+
+import androidx.compose.runtime.*
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun BorrameListScreen () {
+
+}
+
+@Composable
+private fun TipoPenalidadEditBody() {
+
+}
+
+@Preview
+@Composable
+private fun borramePreview() {
+    
+} 
