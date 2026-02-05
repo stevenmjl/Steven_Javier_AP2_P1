247abf3d606e1a07e8a479ef081cc91b39f580f7
stevenmjl <stevenmjl.24@gmail.com>
2026-02-04 20:43:01 -0400

    Agregando NavHost.kt

--- app/src/main/java/edu/ucne/steven_javier_ap2_p1/presentation/NavHost.kt
+++ app/src/main/java/edu/ucne/steven_javier_ap2_p1/presentation/NavHost.kt
@@ -5,8 +5,8 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import edu.ucne.steven_javier_ap2_p1.presentation.entidad.edit.BorrameEditScreen
-import edu.ucne.steven_javier_ap2_p1.presentation.entidad.list.BorrameListScreen
+//import edu.ucne.steven_javier_ap2_p1.presentation.entidad.edit.BorrameEditScreen
+//import edu.ucne.steven_javier_ap2_p1.presentation.entidad.list.BorrameListScreen
 
 sealed class Pantalla(val ruta: String) {
     data object Lista : Pantalla("lista_borrame")
@@ -21,16 +21,16 @@ fun AppNavigation(
         navController = navController,
         startDestination = Pantalla.Lista.ruta
     ) {
-        composable(Pantalla.Lista.ruta) {
-            BorrameListScreen(
-                onIrAEditar = { navController.navigate(Pantalla.Editar.ruta) }
-            )
-        }
-
-        composable(Pantalla.Editar.ruta) {
-            BorrameEditScreen(
-                onVolver = { navController.popBackStack() }
-            )
-        }
+//        composable(Pantalla.Lista.ruta) {
+//            BorrameListScreen(
+//                onIrAEditar = { navController.navigate(Pantalla.Editar.ruta) }
+//            )
+//        }
+//
+//        composable(Pantalla.Editar.ruta) {
+//            //BorrameEditScreen(
+//                onVolver = { navController.popBackStack() }
+//            )
+//        }
     }
 }
