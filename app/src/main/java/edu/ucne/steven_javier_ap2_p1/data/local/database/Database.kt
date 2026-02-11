490310850d729af2222f9f94ce7d4a9d91fa0103
stevenmjl <stevenmjl.24@gmail.com>
2026-02-11 19:15:08 -0400

    Mofificando Database.kt

--- app/src/main/java/edu/ucne/steven_javier_ap2_p1/data/local/database/Database.kt
+++ app/src/main/java/edu/ucne/steven_javier_ap2_p1/data/local/database/Database.kt
@@ -2,9 +2,12 @@ package edu.ucne.steven_javier_ap2_p1.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import edu.ucne.steven_javier_ap2_p1.data.local.entities.CervezaEntity
+import edu.ucne.steven_javier_ap2_p1.data.local.dao.CervezaDao
 
-@Database(entities = [BorrameEntity::class], version = 1, exportSchema = false)
+@Database(entities = [CervezaEntity::class], version = 1, exportSchema = false)
 
 abstract class Database : RoomDatabase() {
+    abstract val cervezaDao: CervezaDao
 
 }
