347d1e427b00834a6f906142fd715ae92c1f5596
stevenmjl <stevenmjl.24@gmail.com>
2026-02-11 18:47:38 -0400

    iniciando examen

--- app/src/main/java/edu/ucne/steven_javier_ap2_p1/data/local/entities/BorrameEntity.kt
+++ app/src/main/java/edu/ucne/steven_javier_ap2_p1/data/local/entities/BorrameEntity.kt
@@ -1,4 +1,10 @@
 package edu.ucne.steven_javier_ap2_p1.data.local.entities
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "borrame")
 class BorrameEntity {
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0
 }
