b839428412748da5c3b69de287ee8f822975ce21
stevenmjl <stevenmjl.24@gmail.com>
2026-02-11 19:20:43 -0400

    Agregagando Cerveza.kt al domain

---
+++ app/src/main/java/edu/ucne/steven_javier_ap2_p1/domain/model/Cerveza.kt
@@ -0,0 +1,12 @@
+package edu.ucne.steven_javier_ap2_p1.domain.model
+
+data class Cerveza (
+
+    val idcerveza: Int? = null,
+    val nombre: String = "",
+    val marca: String = "",
+    val puntuacion: Int = 0,
+)
+
+
+
