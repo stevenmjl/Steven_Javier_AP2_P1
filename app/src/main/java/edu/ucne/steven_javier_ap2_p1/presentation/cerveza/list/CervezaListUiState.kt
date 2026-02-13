0d6ee552cdf235c7d45687056ef2c4ccf6a063e2
stevenmjl <stevenmjl.24@gmail.com>
2026-02-12 22:45:35 -0400

    Funcionalidad de filtros en CervezaListUiState.kt

--- app/src/main/java/edu/ucne/steven_javier_ap2_p1/presentation/cerveza/list/CervezaListUiState.kt
+++ app/src/main/java/edu/ucne/steven_javier_ap2_p1/presentation/cerveza/list/CervezaListUiState.kt
@@ -5,6 +5,11 @@ import edu.ucne.steven_javier_ap2_p1.domain.model.Cerveza
 data class CervezaListUiState(
     val isLoading: Boolean = false,
     val cervezas: List<Cerveza> = emptyList(),
+    val cervezasFiltradas: List<Cerveza> = emptyList(),
+    val filtroNombre: String = "",
+    val filtroMarca: String = "",
+    val conteoFiltrado: Int = 0,
+    val promedioPuntuacion: Double = 0.0,
     val message: String? = null,
     val navigateToCreate: Boolean = false,
     val navigateToEditId: Int? = null
